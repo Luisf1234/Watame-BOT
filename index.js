dddedf98c257ce556129371f06805b273a73088e
Reirel <81500953+Reirel@users.noreply.github.com>
2022-04-04 15:33:27 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1764,10 +1764,11 @@ if (isBanned) return  reply(mess.banned)
 if (!isQuotedSticker) return reply('Responde a un sticker!')
 encmedia = JSON.parse(JSON.stringify(mek).replace('quotedM','m')).message.extendedTextMessage.contextInfo
 media = await cnf.downloadAndSaveMediaMessage(encmedia)
+ran = fs.readFileSync(`./media/toimg.png`)
 exec(`ffmpeg -i ${media} ${ran}`, (err) => {
 fs.unlinkSync(media)
 if (err) return reply('Hubo un fallo, por favor intentalo de nuevo 😿')
-buffer = await getBuffer(ran)
+buffer = fs.readFileSync(ran)
 fakethumb(buffer,'Listo 🙂')
 fs.unlinkSync(ran)
 })
